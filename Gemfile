542d077ad579136eda7c0150cf1cb0a726f892d5
Ian Carrico <git@iancarrico.com>
2014-11-20 12:29:04 -0600

    Initial commit

---
+++ Gemfile
@@ -0,0 +1,16 @@
+# Pull gems from RubyGems
+source 'https://rubygems.org'
+
+# Our Sass stack
+gem 'compass', "~> 1.0"
+gem 'sass', "~>3.4"
+
+gem 'jekyll', "~>2.5"
+
+group :jekyll_plugins do
+  gem 'jekyll-minify-html'
+  
+  # Asset Pipline
+  gem 'jekyll-assets'
+  gem 'yui-compressor'
+end
---
+++ gulpfile.js
@@ -0,0 +1,57 @@
+var gulp = require('gulp');
+var $ = require('gulp-load-plugins')();
+var cp = require('child_process');
+var runSequence = require('run-sequence');
+
+
+
+//////////////////////////////
+// Sass compile Task
+//////////////////////////////
+gulp.task('sass', function() {
+  return gulp.src('_sass/style.scss')
+    .pipe($.compass({
+      config_file: './config.rb',
+      css: 'css',
+      sass: '_sass',
+      bundle_exec: true,
+      time: true
+    }))
+    .pipe($.autoprefixer("last 2 versions", "> 1%"))
+    .pipe(gulp.dest('css'));
+});
+
+//////////////////////////////
+// Jekyll Tasks
+//////////////////////////////
+gulp.task('jekyll-build', function (done) {
+  return cp.spawn('bundle', ['exec', 'jekyll', 'build'], {stdio: 'inherit'})
+    .on('close', done);
+});
+
+gulp.task('jekyll', ['jekyll-build']);
+
+
+//////////////////////////////
+// Publishing Task
+//////////////////////////////
+gulp.task('gh-pages', function () {
+  return gulp.src("./_site/**/*")
+    .pipe(deploy({
+      cacheDir: '.tmp'
+    })).pipe(gulp.dest('/tmp/gh-pages')); ;
+});
+
+
+
+//////////////////////////////
+// Deploy Task
+//////////////////////////////
+gulp.task('deploy', function(cb) {
+  return runSequence(
+    'sass',
+    'jekyll-build',
+    'gh-pages',
+    cb
+  );
+});
